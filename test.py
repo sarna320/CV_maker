0106f589e98aca9eb8df56ff822de56ce5fb558a
Pawel <sarnacki99@wp.pl>
2024-06-16 20:29:04 +0200

    test refactor

--- test.py
+++ test.py
@@ -11,14 +11,7 @@ from requests.exceptions import SSLError
 llm = ChatOllama(model="llama3",base_url="https://c66e-34-72-185-215.ngrok-free.app")
 prompt = ChatPromptTemplate.from_template("Tell me a short joke about {topic}")
 
-# using LangChain Expressive Language chain syntax
-# learn more about the LCEL on
-# /docs/concepts/#langchain-expression-language-lcel
 chain = prompt | llm | StrOutputParser()
-
-# for brevity, response is printed in terminal
-# You can use LangServe to deploy your application for
-# production
 
 def invoke_chain():
     try:
